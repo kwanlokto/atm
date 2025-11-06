5c57a7ff2776cc75f60fb6039bf680129bd7c59f
Ray Kwan <rkwan@skatescribe.com>
2025-11-06 12:21:41 -0500

    use email type

--- frontend/src/routes/SignUp.js
+++ frontend/src/routes/SignUp.js
@@ -11,8 +11,13 @@ import { makeStyles } from '@mui/styles';
 import { userSignup } from '../data-handler/auth';
 
 const useStyles = makeStyles(() => ({
-  paper: { marginTop: 8, display: 'flex', flexDirection: 'column', alignItems: 'center' },
-  form: { width: '100%', marginTop: 1 }
+  paper: {
+    marginTop: 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  form: { width: '100%', marginTop: 1 },
 }));
 
 export const SignUp = () => {
@@ -42,23 +47,72 @@ export const SignUp = () => {
     <Container component="main" maxWidth="xs">
       <Box className={classes.paper}>
         <Avatar sx={{ m: 1 }} />
-        <Typography component="h1" variant="h5">Sign up</Typography>
-        <Box component="form" onSubmit={signUp} className={classes.form} noValidate>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={signUp}
+          className={classes.form}
+          noValidate
+        >
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
-              <TextField autoComplete="given-name" name="firstName" required fullWidth label="First Name" autoFocus value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+              <TextField
+                autoComplete="given-name"
+                name="firstName"
+                required
+                fullWidth
+                label="First Name"
+                autoFocus
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+              />
             </Grid>
             <Grid item xs={12} sm={6}>
-              <TextField required fullWidth label="Last Name" name="lastName" autoComplete="family-name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+              <TextField
+                required
+                fullWidth
+                label="Last Name"
+                name="lastName"
+                autoComplete="family-name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField required fullWidth label="Email Address" name="email" autoComplete="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+              <TextField
+                required
+                fullWidth
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Grid>
             <Grid item xs={12}>
-              <TextField required fullWidth label="Password" name="password" type="password" autoComplete="new-password" value={password} onChange={(e) => setPassword(e.target.value)} />
+              <TextField
+                required
+                fullWidth
+                label="Password"
+                name="password"
+                type="password"
+                autoComplete="new-password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </Grid>
           </Grid>
-          <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>Sign Up</Button>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign Up
+          </Button>
           <Grid container justifyContent="flex-end">
             <Grid item>
               <Link to="/login">Already have an account? Sign in</Link>
